20d5ee2623eae49285b3a300fa46178085c1f6c1
80HD-0 <afunnierguy@gmail.com>
2025-09-15 19:14:05 -0700

    Fix spelling errors in prints, add filename request/arg, update version

--- mnc.c
+++ mnc.c
@@ -20,19 +20,22 @@ const int maxtries = 3;
 int tries = maxtries;
 int ok = 1;
 int i = 0;
+FILE *fp;
 uint32_t packetindex = 0;
 char outpacket[2052] = {0};
 char readbuffer[2048] = {0};
 char *address = "127.0.0.1";
+char *filename = "test.txt";
 int main(int argc, char *argv[]) {
     int opt;
     int version = 0;
-    int release = 0;
-    int subrelease = 3;
-    while ((opt = getopt(argc, argv, "a:h")) != -1) {
+    int release = 1;
+    int subrelease = 0;
+    while ((opt = getopt(argc, argv, "f:a:h")) != -1) {
         switch (opt) {
-            case 'h': printf("MeshNetClient %i.%i.%i Help\n[-h]: Display this help menu.\n[-a (address)]: Set a server address to pull from (defualt 127.0.0.1)\n\n", version, release, subrelease); return 0;
+            case 'h': printf("MeshNetClient %i.%i.%i Help\n[-h]: Display this help menu.\n[-a (address)]: Set a server address to pull from (default 127.0.0.1)\n[-f (filename)]: Gets that file from the server (default test.txt)\n\n", version, release, subrelease); return 0;
             case 'a': address = optarg; break;
+            case 'f': filename = optarg; break;
         }
     }
     struct sigaction sa;
@@ -58,15 +61,21 @@ int main(int argc, char *argv[]) {
     server_addr.sin_port = htons(5960);
     printf("Pulling from UDP %s:5960\n", address);
     socklen_t addr_len = sizeof(server_addr);
-    sendto(sock, "GET", 3, 0, (struct sockaddr*)&server_addr, addr_len);
-    FILE *fp = fopen("output.txt", "ab");
-    if (!fp) { perror("fopen"); return 1; }
+    char request[256];
+    snprintf(request, sizeof(request), "GET,%s", filename);
+    sendto(sock, request, strlen(request), 0, (struct sockaddr*)&server_addr, addr_len);
+    if (access(filename, F_OK) == -1) {
+        fp = fopen(filename, "ab");
+        if (!fp) { perror("fopen"); return 1; }
+    } else {
+        printf("That file already exists! Delete it first before running.");
+        return(1);
+    }
     gettimeofday(&start_time, NULL);
     while (1) {
         char packet[2052];
         //its time for the big mamaaa
         ssize_t recv_len = recvfrom(sock, packet, sizeof(packet), 0, (struct sockaddr*)&server_addr, &addr_len);
-        filesize += recv_len;
         if (recv_len < 0) {
             printf("Packet length less than 0");
             continue;
@@ -84,10 +93,11 @@ int main(int argc, char *argv[]) {
         strcpy(outpacket, sig);
         sendto(sock, outpacket, strlen(sig), 0, (struct sockaddr*)&server_addr, addr_len);
         if (strstr(packet, "Net Connection Established") != NULL ||
-            strncmp(packet, "GET Recieved.", 13) == 0) {
+            strstr(packet, "Prepared to send") != NULL) {
             continue; // skip these header packets
         }
         fwrite(packet + 4, 1, recv_len - 4, fp);
+        filesize += recv_len;
     }
     return(0);
 }
